62c08f402727b85e4cb8e23753dfa644ffd8abdb
sajkm <sajkm321@gmail.com>
2023-12-19 20:26:06 +0530

    backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -17,7 +17,7 @@ export const signUp = async (req, res, next) => {
 
 export const signIn = async (req, res, next) => {
   const { email, password } = req.body;
-
+ console.log(email,password);
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found!'));
